ea8b4183a7c88d14e04dcac42a2b46730f8a3eb1
alebautt <id_bautista@hotmail.com>
2015-02-06 18:52:51 -0600

    Initial Commit

---
+++ OaxacaPush/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OaxacaPush
+//
+//  Created by Alejandra B on 06/02/15.
+//  Copyright (c) 2015 alebautista. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OaxacaPush/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OaxacaPush
+//
+//  Created by Alejandra B on 06/02/15.
+//  Copyright (c) 2015 alebautista. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
